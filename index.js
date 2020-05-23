0c7af396ebf55531a386a4cd5db1f77b2126caee
devHazz <44245035+devHazz@users.noreply.github.com>
2020-05-23 12:44:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const remote = require('./Controllers/BaseController.js')
 
 
-const control = new remote('192.168.0.48')
-
-console.log(remote.commands)
-
-control.press('home');
+const control = new remote('192.168.x.x')
+control.press('home'); //Presses the home button
